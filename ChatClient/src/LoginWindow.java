21b2930571b5d5e32e6d120845f50dd8899f0ece
Krzysztof Wieckowski <krzysztof.wieckowski@ericsson.com>
2019-01-24 11:43:47 +0100

    Wyglad okna logowania

--- ChatClient/src/LoginWindow.java
+++ ChatClient/src/LoginWindow.java
@@ -1,22 +1,32 @@
 import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.IOException;
 
-import javax.swing.BoxLayout;
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JPasswordField;
 import javax.swing.JTextField;
+import javax.swing.border.LineBorder;
 
 public class LoginWindow extends JFrame {
 
 	JTextField loginField = new JTextField();
 	JPasswordField passwordField = new JPasswordField();
+	
+    private JLabel lbUsername;
+    private JLabel lbPassword;
+	
 	JButton loginButton = new JButton("Login");
 	private ChatClient client;
+	
+	
 	
 	public LoginWindow() {
 		super("Chat Client");
@@ -27,14 +37,75 @@ public class LoginWindow extends JFrame {
 
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
-		JPanel p = new JPanel();
-		p.setLayout((new BoxLayout(p, BoxLayout.Y_AXIS)));
+		JPanel p = new JPanel(new GridBagLayout());
 		
-		p.add(loginField);
-		p.add(passwordField);
-		p.add(loginButton);
+		
+		
+		GridBagConstraints cs = new GridBagConstraints();
+		cs.fill = GridBagConstraints.HORIZONTAL;
+		
+		
+		
+		
+		
+		
+		
+		lbUsername = new JLabel("Nick: ");
+        cs.gridx = 0;
+        cs.gridy = 0;
+        cs.gridwidth = 1;
+        p.add(lbUsername, cs);
+ 
+        loginField = new JTextField(20);
+        cs.gridx = 1;
+        cs.gridy = 0;
+        cs.gridwidth = 2;
+        p.add(loginField, cs);
+		
+		
+		
+        lbPassword = new JLabel("Password: ");
+        cs.gridx = 0;
+        cs.gridy = 1;
+        cs.gridwidth = 1;
+        p.add(lbPassword, cs);
+		
+		
+        passwordField = new JPasswordField(20);
+        cs.gridx = 1;
+        cs.gridy = 1;
+        cs.gridwidth = 2;
+        p.add(passwordField, cs);
+        p.setBorder(new LineBorder(Color.GRAY));
+		
+		
+        JPanel bp = new JPanel();
+		
+		
+		
+		bp.add(loginButton);
+		
+		
+		
+		getContentPane().add(p, BorderLayout.CENTER);
+        getContentPane().add(bp, BorderLayout.PAGE_END);
+ 
+        pack();
+        setResizable(false);
+       // setLocationRelativeTo(parent);
+		
+		
+		////// Formatowanie okna logowania
+		
+		
+		
+		
+		
 		
 		//
+		//
+        //loginButton.setBackground(new Color(0, 128, 64));
+        
 		loginButton.addActionListener(new ActionListener() {
 
 			@Override
@@ -80,6 +151,13 @@ public class LoginWindow extends JFrame {
 				frame.getContentPane();
 				frame.add(userPanel, BorderLayout.CENTER);
 				frame.setVisible(true);
+				
+				
+				
+				
+				
+				
+				
 				
 				// test			- zmienic dostep do userListModel w UserPanel na private
 				//userPanel.userListModel.addElement("user");
